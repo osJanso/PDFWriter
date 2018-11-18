e49b330b6d49c524f7614de871b51902a11746ed
osJanso <34491171+osJanso@users.noreply.github.com>
2018-11-18 13:38:51 +0100

    added Lib. Dontknow how to get further

---
+++ src/PDf.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class PDf {
+
+	
+	public static final String DEST = "C:/User/Jan/Desktop/Test.pdf";
+	
+	public static void main(String[] args) {
+		File file = new File(DEST);
+		PDFWriter writer = new PDFWriter();
+	
+		
+
+	}
+
+}
